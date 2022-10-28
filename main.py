d8d0c75929e9baf5a9794da1a5c96d7b7bec5080
nordson <21nordson@gmail.com>
2022-10-28 12:47:52 +0300

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 test_url = 'https://google.com'
 url = 'https://fantasy-worlds.org/lib/-genre:13/'
-response = requests.get(test_url)
+response = requests.get(url)
 response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
-print(soup)
+all_books = soup.find_all('table', {"class": "news_body"})
