cc0c5e8248fe84800f50542342c059408c55357f
nordson <21nordson@gmail.com>
2022-10-28 11:12:26 +0300

    test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-
+test_url = 'https://google.com'
 url = 'https://fantasy-worlds.org/lib/-genre:13/'
-response = requests.get(url)
+response = requests.get(test_url)
 response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
 print(soup)
