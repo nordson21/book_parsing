acce461893f17c8400b2198f0785d63cc1559db0
nordson <21nordson@gmail.com>
2022-10-28 10:46:17 +0300

    test

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import requests
 
 url = 'https://fantasy-worlds.org/lib/-genre:13/'
 response = requests.get(url)
-print(response)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+print(soup)
